7181f20a9f4d88b3c3a434f5fc746aa437549c58
Arpit-Jaiswal20 <arpitjaiswalaj9@gmail.com>
2025-02-27 12:59:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+document.getElementById("change_button").addEventListener("click", function() {
+            let blockId = document.getElementById("block_id").value;
+            let color = document.getElementById("colour_id").value;
+            
+            document.querySelectorAll(".grid-item").forEach(item => {
+                item.style.backgroundColor = "transparent";
+            });
+            
+            let selectedBlock = document.getElementById(blockId);
+            if (selectedBlock) {
+                selectedBlock.style.backgroundColor = color;
+            }
+        });
+
+document.getElementById("Reset").addEventListener("click", function() {
+            document.querySelectorAll(".grid-item").forEach(item => {
+                item.style.backgroundColor = "transparent";
+            });
+        });
