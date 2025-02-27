aded381e73cb85d9aab59305d500ba9e6f268b50
Arpit-Jaiswal20 <arpitjaiswalaj9@gmail.com>
2025-02-27 13:02:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ document.getElementById("change_button").addEventListener("click", function() {
             let blockId = document.getElementById("block_id").value;
             let color = document.getElementById("colour_id").value;
             
-            document.querySelectorAll(".grid-item").forEach(item => {
-                item.style.backgroundColor = "transparent";
-            });
+            // document.querySelectorAll(".grid-item").forEach(item => {
+            //     item.style.backgroundColor = "transparent";
+            // });
             
             let selectedBlock = document.getElementById(blockId);
             if (selectedBlock) {
